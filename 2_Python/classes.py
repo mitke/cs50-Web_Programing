64431d6f3fc44f484ce2e5c8ec3994d4fd7ef4e8
Mitke <miodrag.mitic@gmail.com>
2023-10-26 14:48:46 +0200

    dodat class.py

---
+++ 2_Python/classes.py
@@ -0,0 +1,24 @@
+class Flight():
+  def __init__(self, capacity):
+    self.capacity = capacity
+    self.passengers = []
+    
+  def add_passenger(self, name):
+    if not self.open_seats():
+      return False
+    self.passengers.append(name)
+    return True
+    
+  def open_seats(self):
+    return self.capacity - len(self.passengers)
+  
+flight_853 = Flight(3)
+
+people = ["Herry", "Ron", "Hermione", "Ginny"]
+
+for person in people:
+  success = flight_853.add_passenger(person)
+  if success:
+    print(f"Added {person} to flight 853 successfully")
+  else:
+    print(f"No available seats for {person}")
